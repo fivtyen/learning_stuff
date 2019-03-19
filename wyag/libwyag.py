5b14b582e2fdd0d8a2db7e08e6c36ec8f251d0f2
fivtyen <5509131+fivtyen@users.noreply.github.com>
2019-03-19 18:23:07 +0100

    update

--- wyag/libwyag.py
+++ wyag/libwyag.py
@@ -15,34 +15,35 @@ argsubparsers.required = True
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
 
-    if   args.command == "add": 
-        cmd_add(args)
+    if  args.command == "add": 
+        pass
+    #     cmd_add(args)
     elif args.command == "cat-file": 
         cmd_cat_file(args)
-    elif args.command == "checkout": 
-        cmd_checkout(args)
-    elif args.command == "commit": 
-        cmd_commit(args)
-    elif args.command == "hash-object": 
-        cmd_hash_object(args)
+    # elif args.command == "checkout": 
+    #     cmd_checkout(args)
+    # elif args.command == "commit": 
+    #     cmd_commit(args)
+    # elif args.command == "hash-object": 
+    #     cmd_hash_object(args)
     elif args.command == "init": 
         cmd_init(args)
-    elif args.command == "log": 
-        cmd_log(args)
-    elif args.command == "ls-tree": 
-        cmd_ls_tree(args)
-    elif args.command == "merge": 
-        cmd_merge(args)
-    elif args.command == "rebase": 
-        cmd_rebase(args)
-    elif args.command == "rev-parse": 
-        cmd_rev_parse(args)
-    elif args.command == "rm": 
-        cmd_rm(args)
-    elif args.command == "show-ref": 
-        cmd_show_ref(args)
-    elif args.command == "tag": 
-        cmd_tag(args)
+    # elif args.command == "log": 
+    #     cmd_log(args)
+    # elif args.command == "ls-tree": 
+    #     cmd_ls_tree(args)
+    # elif args.command == "merge": 
+    #     cmd_merge(args)
+    # elif args.command == "rebase": 
+    #     cmd_rebase(args)
+    # elif args.command == "rev-parse": 
+    #     cmd_rev_parse(args)
+    # elif args.command == "rm": 
+    #     cmd_rm(args)
+    # elif args.command == "show-ref": 
+    #     cmd_show_ref(args)
+    # elif args.command == "tag": 
+    #     cmd_tag(args)
 
 
 class GitRepository():
@@ -178,12 +179,12 @@ class GitObject():
         if data != None:
             self.deserialize(data)
 
-
+    
     def serialize(self):
         """Implemented by subclasses"""
         raise Exception("Unimplemented!")
 
-
+    
     def deserialize(self, data):
         raise Exception("Unimplemented!")
 
@@ -203,18 +204,19 @@ def object_read(repo, sha):
         if size != len(raw)-y-1:
             raise Exception("Malformed object %s: bad length!" % sha)
         
-        if fmt ==b'commit':
-            c = GitCommit
-        if fmt ==b'tree':
-            c = GitTree
-        if fmt ==b'tag':
-            c = GitTag
+        # if fmt ==b'commit':
+        #     c = GitCommit
+        # if fmt ==b'tree':
+        #     c = GitTree
+        # if fmt ==b'tag':
+        #     c = GitTag
         if fmt == b'blob':
             c = GitBlob
         else:
             raise Exception("Unknown type %s for object %s" %(fmt.decode("ascii"), sha))
 
         return c(repo, raw[y+1:])
+
 
 # TODO, for now a placeholder
 def object_find(repo, name, fmt=None, follow=True):
@@ -233,3 +235,178 @@ def write_object(obj, actually_write=True):
             f.write(zlib.compress(result))
 
     return sha
+
+
+class GitBlob(GitObject):
+    fmt = b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+
+argsp = argsubparsers.add_parser("cat-file", help="Provide content of repo objects")
+
+argsp.add_argument("type",
+                   metavar="type",
+                   choices=["blob", "commit", "tag", "tree"],
+                   help="Specify the type")
+
+argsp.add_argument("object",
+                   metavar="object",
+                   help="The object to display")
+
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer(obj.serialize())
+
+
+argsp = argsubparsers.add_parser("hash-object", 
+                                 help="Compute object ID, optionally create a blob")
+
+argsp.add_argument("-t",
+                   metavar="type",
+                   dest="type",
+                   choices=["blob", "commit", "tree", "tag"],
+                   default="blob",
+                   help="Specify the type")
+
+argsp.add_argument("-w",
+                   dest="write",
+                   action="store_true",
+                   help="Write the object into the database")
+
+argsp.add_argument("path",
+                   help="Read object from <file>")
+
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = GitRepository(".")
+    else:
+        repo = None
+    
+    with open(args.path, "rb") as f:
+        sha = object_hash(f, args.type.encode(), repo)
+        print(sha)
+
+
+def object_hash(f, fmt, repo=None):
+    data = f.read()
+
+    if fmt == b'commit':
+        obj = GitCommit(repo, data)
+    if fmt == b'tree':
+        obj = GitTree(repo, data)
+    if fmt == b'tag':
+        obj = GitTag(repo, data)
+    if fmt == b'blob':
+        obj = GitBlob(repo, data)
+    else:
+        raise Exception("Unknown type %s" % fmt)
+
+    return object_write(obj, repo)
+
+
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = collections.OrderedDict()
+
+    spc = raw.find(b' ', start)
+    nl = raw.find(b'\n', start)
+    
+    if spc < 0 or nl < spc:
+        assert(nl == start)
+        dct[b''] == raw[start+1]
+        return dct
+
+    key = raw[start:spc]
+
+    end = start
+    while True:
+        end = raw.find(b'\n', end+1)
+        if raw[end+1] != ord(' '):
+            break
+
+    value = raw[spc+1:end].replace(b'\n ', b'\n')
+    
+    if key in dct:
+        if type(dct[key]) == list:
+            dct[key].append(value)
+        else:
+            dct[key] = [dct[key], value]
+    else:
+        dct[key] = value
+        
+    return kvlm_parse(raw, start=end+1, dct=dct)
+
+
+def kvlm_serialize(kvlm):
+    ret = b''
+    for k in kvlm.keys():
+        if k == b'':
+            continue
+        val = kvlm[k]
+    if type(val) != list:
+        val = [val]
+
+    for v in val:
+        ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
+
+    ret += b'\n' + kvlm[b'']
+
+    return ret
+
+
+class GitCommit(GitObject):
+    fmt = b'commit'
+
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
+
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+
+
+argsp.argsubparsers.add_parser("log", help="Display commit log")
+argsp.add_argument("commit",
+                  default="HEAD",
+                  nargs="?",
+                  help="Commit to start at")
+
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+def log_graphviz(repo, sha, seen):
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    assert commit.fmt == b'commit'
+
+    if not b'parent' in commit.kvlm.keys():
+        return
+    
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list:
+        parents = [parents]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print("c_{} -> c_{}".format(sha, p))
+        log_graphviz(repo, p, seen)
